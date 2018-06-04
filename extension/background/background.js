599f2acd940c55d592437fe717cccdc89e69feac
terrygonguet <terry@gonguet.com>
2018-06-04 02:14:22 +0200

    I think I fixed the dead object and storage sync issues. Started work on popup structure.

--- extension/background/background.js
+++ extension/background/background.js
@@ -8,13 +8,45 @@ browser.storage.sync.get()
     default_increase:1,
     include_URL_with_selection: true,
     default_tick_every: { days:1 },
+    selected: null,
   };
-  projects = _.omit(data, 'settings');
+  projects = data.projects || {};
 });
 
-function saveData() {
-  return browser.storage.set(Object.assign({ settings }, projects));
+function saveData(newProjects, newSettings) {
+  let data = { projects, settings };
+  if (newProjects) {
+    data.projects = JSON.parse(JSON.stringify(newProjects)); // ewwww
+    projects = data.projects;
+  }
+  if (newProjects) {
+    data.settings = JSON.parse(JSON.stringify(newSettings)); // ewwww
+    settings = data.settings;
+  }
+  return browser.storage.sync.set(data);
 }
+
+function createProject(props={}) {
+  let project = Project.create(
+    Object.assign({
+      default_increase: settings.default_increase,
+      default_priority: settings.default_priority,
+      tick_every: settings.default_tick_every
+    }, props)
+  );
+  projects[project.id] = project;
+  if (!settings.selected) settings.selected = project.id;
+  return saveData();
+}
+
+Object.defineProperty(window, 'selected', {
+  get() {
+    return projects[settings.selected];
+  },
+  set(value) {
+    settings.selected = (typeof value === 'string' ? value : value.id);
+  }
+});
 
 // browser.contextMenus.create({
 //   id: "add-link",
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,3 +1,28 @@
 var bg;
 browser.runtime.getBackgroundPage()
-.then(win => { bg = win; }, console.error);
+.then(win => {
+  bg = win;
+  let app = new Vue({
+    el:'#container',
+    data: {
+      bg,
+      projects: _.cloneDeep(bg.projects),
+      settings: _.cloneDeep(bg.settings),
+      selectedTask: null,
+    },
+    methods: {
+      createProject() {
+        bg.createProject({
+          name:'Test'+Math.floor(Math.random() * 500)
+        }).then(() => {
+          app.projects = _.cloneDeep(bg.projects);
+          app.settings = _.cloneDeep(bg.settings);
+        });
+      }
+    }
+  });
+
+  window.addEventListener('unload', () => {
+    bg.saveData(app.projects, app.settings);
+  });
+}, console.error);
